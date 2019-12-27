9fca2a2e62647247eeff221c7a4d6edfe70b28f8
Abdul Rehman <abdul-rehman@myseneca.ca>
2019-12-27 18:53:29 -0500

    inital footer build

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -1,10 +1,65 @@
-import React from "react";
+import React, { Fragment } from "react";
+import { Grid, Fab } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import TwitterIcon from '@material-ui/icons/Twitter';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import "./footer.css";
 
+const useCSSStyles = makeStyles(theme => ({
+    icon: {
 
-export default class Footer extends React.Component {
-    render() {
-        return (
-            <h1>Footer</h1>
-        )
+    },
+    grid: {
+        backgroundColor: '#8BC6EC',
+        backgroundImage: 'linear-gradient(135deg, #8BC6EC 0%, #9599E2 100%)',
+        height: 100,
+        padding: 10,
+        boxShadow: '0 3px 5px 2px rgba(139, 198, 236, .8)',
+    },
+    gridItem: {
+        textAlign: "center"
+    },
+    footerGrid:{
+        height: '10vh'
+    },
+    para: {
+        backgroundColor: '#8BC6EC'
     }
+
+}));
+
+export default function FloatingActionButtons() {
+    const cssClasses = useCSSStyles();
+
+    return (
+        <Fragment>
+            <Grid container spacing={3} className={cssClasses.grid} justify="center" wrap="wrap">
+                <Grid item>
+                    <p>follow me on: </p>
+                </Grid>
+                <Grid item md={1} sm={1} xs={2} className={cssClasses.gridItem}>
+                    <a href="https://github.com/Abdul-Sen"> {/* href in FAB does not redirect externally because of react router */}
+                    <Fab variant="round" size="small" color="primary">
+                        <GitHubIcon ></GitHubIcon>
+                    </Fab>
+                    </a>
+                </Grid>
+                <Grid item md={1} sm={1} xs={2} className={cssClasses.gridItem}>
+                    <a href="https://www.linkedin.com/in/abdul-r/">
+                    <Fab variant="round" size="small" color="primary">
+                        <LinkedInIcon></LinkedInIcon>
+                    </Fab>
+                    </a>
+                </Grid>
+                <Grid item md={1} sm={1} xs={2} className={cssClasses.gridItem}>
+                    <a href="https://twitter.com/AbdulRe47817062">
+                    <Fab variant="round" size="small" color="primary">
+                        <TwitterIcon></TwitterIcon>
+                    </Fab>
+                    </a>
+                </Grid>
+            </Grid>
+        </Fragment>
+    )
 }
