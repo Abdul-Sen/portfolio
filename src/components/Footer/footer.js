cc2cd118f804d55b5d5a0c11f69c1fa0afd17196
Abdul Rehman <abdul-rehman@myseneca.ca>
2019-12-28 16:29:43 -0500

    publish config setup complete

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -13,7 +13,7 @@ const useCSSStyles = makeStyles(theme => ({
     grid: {
         backgroundColor: '#8BC6EC',
         backgroundImage: 'linear-gradient(135deg, #8BC6EC 0%, #9599E2 100%)',
-        height: 100,
+        height: 80,
         padding: 10,
         boxShadow: '0 3px 5px 2px rgba(139, 198, 236, .8)',
     },
@@ -34,7 +34,7 @@ export default function FloatingActionButtons() {
 
     return (
         <Fragment>
-            <Grid container spacing={0} className={cssClasses.grid} justify="center" wrap="wrap">
+            <Grid container spacing={0} className={cssClasses.grid} justify="center" alignItems="baseline" wrap="wrap">
                 <Grid item>
                     <p>follow me on: </p>
                 </Grid>
