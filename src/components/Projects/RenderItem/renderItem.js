3f866970dee0f168a6c6d7e8c73031c558c8598d
Abdul Rehman <abdul-rehman@myseneca.ca>
2020-01-01 17:19:57 -0500

    fancy card CSS

--- src/components/Projects/RenderItem/renderItem.js
+++ src/components/Projects/RenderItem/renderItem.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import { ListItem, Card, Box, Grid, Button } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, styled } from '@material-ui/core/styles';
 import { flexbox } from '@material-ui/system';
 import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
 
+
 const cssStyles = makeStyles(theme => ({
+    card: {
+        boxShadow: "0 5px 10px rgba(154,160,185,.05), 0 15px 40px rgba(166,173,201,.2)",
+         transform: "scale(0.91)",
+        "&:hover": {
+            boxShadow: "1px 8px 17px grey",
+            transform: "scale(0.93) translate(10px, -10px)",
+            WebkitTransition: "all .7s ease"
+          }
+    },
     boxContainer: {
         backgroundColor: "red",
         flexDirection: "row",
@@ -24,8 +34,8 @@ const cssStyles = makeStyles(theme => ({
 function RenderItem(props) {
     const useStyle = cssStyles();
     return (
-        <ListItem>
-            <Card >
+        <ListItem >
+            <Card className={useStyle.card}>
                 <Grid container direction="row">
                     <Grid item md={3} lg={3} sm={12} xs={12} className={useStyle.image}>
                         <img className={useStyle.image} src={process.env.PUBLIC_URL + props.project.tileTile.gif} width="200px" height="200px"></img>
