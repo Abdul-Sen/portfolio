ef69e62e6e45dfc52f0cb5691439f6c0a4249af4
Abdul Rehman <abdul-rehman@myseneca.ca>
2020-01-06 15:16:29 -0500

    bug fix for mobile viewport

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -4,6 +4,9 @@ import "./home.css";
 import codingImage from './Coding.png';
 import codingImage2 from './Coding2.png';
 
+
+// ****** NOTE: All other components were migrated to react functions. Home will soon be converted from class component to function component as well*********** 
+
 class Home extends React.Component {
 
   constructor(props) {
@@ -17,15 +20,15 @@ class Home extends React.Component {
       <Fragment>
 
         <Grid spacing={0} container wrap="wrap" justify="space-evenly" direction="row">
-          <Grid item sm={12} md={12} lg={12} xl={12}>
+          <Grid item sm={12} xs={12}   md={12} lg={12} xl={12}>
             <div id="landingOverlay">
               <div id="landing">
                 <Grid spacing={0} container justify="flex-start" direction="column">
                   <Grid item md={3}>
-                    <h1>Welcome!</h1>
+                    <h1>Abdul Rehman</h1>
                   </Grid>
                   <Grid item md={6}>
-                    <h4 >Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nesciunt, minus?</h4>
+                    <h4>...Building cool stuff is my passion</h4>
                   </Grid>
                 </Grid>
               </div>
@@ -35,14 +38,14 @@ class Home extends React.Component {
 
         <Container maxWidth="lg">
           <Grid container spacing={2} direction="row" justify="space-around" >
-            <Grid item md={4} sm={12} xs={12} ><p>Lorem ipsum dolor, sit eaque aliquid, eius rerum blanditiis non dicta nesciunt. Ipsam labore nobis dolorem delectus dolore saepe, amet assumenda ducimus aliquam fugiat sit placeat voluptatibus aperiam veniam quasi nemo voluptas quae cum. Veniam, placeat possimus debitis fugit doloribus, in natus minus ullam, nobis illo quidem numquam facere facilis obcaecati rem aut asperiores quisquam quae voluptates reiciendis ratione error libero? Similique id sit ad optio voluptatum nostrum provident adipisci exercitationem repellat, assumenda quisquam saepe officia error nisi?</p></Grid>
+            <Grid item md={4} sm={12} xs={12}><h3>Determination</h3><p>I am a determined individual. Lorem ipsum dolor, sit eaque aliquid, eius rerum blanditiis non dicta nesciunt. Ipsam labore nobis dolorem delectus dolore saepe, amet assumenda ducimus aliquam fugiat sit placeat voluptatibus aperiam veniam quasi nemo voluptas quae cum. Veniam, placeat possimus debitis fugit doloribus, in natus minus ullam, nobis illo quidem numquam facere facilis obcaecati rem aut asperiores quisquam quae voluptates reiciendis ratione error libero? Similique id sit ad optio voluptatum nostrum provident adipisci exercitationem repellat, assumenda quisquam saepe officia error nisi?</p></Grid>
             <Grid className="img-container"item md={4} sm={12} xs={12} >
               <img src={codingImage} width="300px" height="300px" alt="illustration#2" wrap="wrap"></img>
             </Grid>
             <Grid  className="img-container" item md={5} sm={12} xs={12}>
               <img src={codingImage2} width="300px" height="300px" ></img>
             </Grid>
-            <Grid item md={5} sm={12} xs={12}><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa architecto consequuntur laborum tenetur, eligendi amet voluptatem enim sapiente adipisci quidem ut similique ad autem consectetur a nulla iusto esse laboriosam nemo molestiae cum vero sequi illo quae! Quasi fuga in nesciunt tenetur vel voluptatibus sed ad nobis laudantium inventore voluptatum eaque, impedit laboriosam asperiores quidem ducimus corrupti quae hic voluptates tempora quis harum unde. Inventore ad incidunt illum at voluptatibus laudantium odit quo sunt veniam libero, quia impedit eum mollitia praesentium modi unde, provident doloremque blanditiis numquam doloribus culpa reiciendis neque vel? Error ullam asperiores hic consequatur consectetur sequi commodi.</p></Grid>
+            <Grid item md={5} sm={12} xs={12}><h3>Collaboration</h3><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa architecto consequuntur laborum tenetur, eligendi amet voluptatem enim sapiente adipisci quidem ut similique ad autem consectetur a nulla iusto esse laboriosam nemo molestiae cum vero sequi illo quae! Quasi fuga in nesciunt tenetur vel voluptatibus sed ad nobis laudantium inventore voluptatum eaque, impedit laboriosam asperiores quidem ducimus corrupti quae hic voluptates tempora quis harum unde. Inventore ad incidunt illum at voluptatibus laudantium odit quo sunt veniam libero, quia impedit eum mollitia praesentium modi unde, provident doloremque blanditiis numquam doloribus culpa reiciendis neque vel? Error ullam asperiores hic consequatur consectetur sequi commodi.</p></Grid>
            
 
           </Grid>
