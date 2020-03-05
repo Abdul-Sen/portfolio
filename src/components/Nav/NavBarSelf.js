371dac8301442dac2255f147188731703a76f8b4
Abdul Rehman <abdul-rehman@myseneca.ca>
2020-03-05 15:46:47 -0500

    navbar home icon change

--- src/components/Nav/NavBarSelf.js
+++ src/components/Nav/NavBarSelf.js
@@ -1,6 +1,6 @@
 import * as ROUTES from '../../routes/routes';
 import React, { Fragment } from 'react';
-import { Button, IconButton, SvgIcon, Grid } from '@material-ui/core';
+import { Button, IconButton, SvgIcon, Grid, Icon } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import { HashLink as LinkTwo } from 'react-router-hash-link';
 
@@ -11,17 +11,16 @@ const cssStyles = makeStyles(theme => ({
             color: "#ffffff",
             fontWeight: "370",
           },
+        '& #imgBtn':{
+            width: "30px",
+            height:"30px",
+            marginTop:"auto",
+            marginBottom:"auto"
+        }
       }
   }
 }));
 
-function HomeButton() {
-    return (<IconButton aria-label="delete">
-        <SvgIcon>
-            <path d="M18.121,9.88l-7.832-7.836c-0.155-0.158-0.428-0.155-0.584,0L1.842,9.913c-0.262,0.263-0.073,0.705,0.292,0.705h2.069v7.042c0,0.227,0.187,0.414,0.414,0.414h3.725c0.228,0,0.414-0.188,0.414-0.414v-3.313h2.483v3.313c0,0.227,0.187,0.414,0.413,0.414h3.726c0.229,0,0.414-0.188,0.414-0.414v-7.042h2.068h0.004C18.331,10.617,18.389,10.146,18.121,9.88 M14.963,17.245h-2.896v-3.313c0-0.229-0.186-0.415-0.414-0.415H8.342c-0.228,0-0.414,0.187-0.414,0.415v3.313H5.032v-6.628h9.931V17.245z M3.133,9.79l6.864-6.868l6.867,6.868H3.133z" />
-        </SvgIcon>
-    </IconButton>);
-}
 export default function NavBarSelf(props) {
     const useStyle = cssStyles();
     return (
@@ -37,7 +36,7 @@ export default function NavBarSelf(props) {
                 <Grid item xs={12} sm={1} md={1} style={{textAlign:"center"}}>
                     <LinkTwo smooth={true} to={ROUTES.HOME} style={{textDecoration: "none"}}>
 
-                        {props.display === "desktop" ? <HomeButton></HomeButton> :
+                        {props.display === "desktop" ?<IconButton><img id="imgBtn" src={process.env.PUBLIC_URL + '/home.svg'}></img></IconButton> :
                             <LinkTwo to={ROUTES.HOME} smooth={true} style={{textDecoration: "none"}}>
                                 <Button  fullWidth>
                                     Home
